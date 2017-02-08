f138e11e844e570074961e547288afe30b3e76cc
Delacannon <delacannon@gmail.com>
2017-02-08 22:49:20 +0100

    Add exp

---
+++ main.js
@@ -0,0 +1,13 @@
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World D-Ocean!')
+})
+
+app.get('/user', function (req, res) {
+  res.send('Hello World W-Ocean USER!')
+})
+
+app.listen(3000, function () {
+  console.log('Magic at 3000!')
+})
